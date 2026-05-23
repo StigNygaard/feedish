c47d3ecf60c46a4f41739df721c206b1ccba42f7
Stig Nygaard <stig@rockland.dk>
2026-05-23 12:25:38 +0200

    trying to fix weird csp issue when redirected

--- main.js
+++ main.js
@@ -16,7 +16,8 @@ import { asuswrtForum } from "./misc/asuswrt.js";
 import { sigmaUK } from "./canon/sigma-uk.js";
 
 let responseHeaders = {
-    'Content-Security-Policy': `default-src 'none' ; script-src 'self' ; connect-src https: ; img-src https: blob: data: ; style-src 'self' ; frame-ancestors 'none' ; form-action 'self' ; base-uri 'none'`,
+    // 'Content-Security-Policy': `default-src 'none' ; script-src 'self' ; connect-src https: ; img-src https: blob: data: ; style-src 'self' ; frame-ancestors 'none' ; form-action 'self' ; base-uri 'none'`,
+    'Content-Security-Policy': `default-src 'self' ; connect-src https: ; img-src https: blob: data: ; frame-ancestors 'none' ; form-action 'self' ; base-uri 'none'`,
     'Referrer-Policy': 'strict-origin-when-cross-origin',
     'X-Content-Type-Options': 'nosniff'
 };
